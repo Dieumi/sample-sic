2d38b84eacd0c29cd1177f6084b3885953456145
Pierre <oxeyo@hotmail.fr>
2016-06-01 17:21:53 +0200

    updated test3

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 var express=require('express');
 var api=express();
-
+var DEFAULT_PORT = 3001;
 api.get('/', function(req,res,next){
 	res.send("hello world");
 });
-console.log("server started on port 3001");
+
+api.get('/contacts',function(req,res,next){
+	res.send();
+
+})
+
+api.get('/contacts/:name',function(req,res,next){
+	res.send();
+
+})
+api.post('/contacts/:name',function(req,res,next){
+	if(req.params.name=== 'exists'){
+		return res.send().status(403);	
+		}
+
+})
+api.get('/contacts/:name',function(req,res,next){
+	res.send();
+
+})
+api.put('/contacts/:name/new',function(req,res,next){
+	res.send();
+
+})
+api.delete('/contacts/:name',function(req,res,next){
+	res.send();
+})
+var port=process.env.PORT || DEFAULT_PORT;
+console.log("server started on port "+port);
 api.listen(3001);
 
 module.exports=api;
